82bf9ba265fab455b95a4ced9446d4564d49e3db
SeptEarlyMorning <1790732099@qq.com>
2020-06-01 23:29:35 +0800

    导航栏完善

--- src/components/leftNav/index.tsx
+++ src/components/leftNav/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import logo from '../../assets/images/logo.png';
 import { Menu } from 'antd';
 import SubMenu from 'antd/lib/menu/SubMenu';
@@ -9,7 +9,7 @@ import {
   ContainerOutlined,
   MailOutlined,
 } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const leftNav = require('./leftNav.module.less');
 
@@ -18,63 +18,74 @@ interface IProps {
 };
 
 function LeftNav(props: IProps) {
+  const location = useLocation();
+  const { pathname } = location;
+  let subKey: string;
+  if (pathname === '/category' || pathname === '/product') {
+    subKey = 'sub1';
+  } else if (pathname === '/chart/bar' || pathname === '/chart/line' || pathname === '/chart/pie') {
+    subKey = 'sub2';
+  } else {
+    subKey = ''
+  }
+
   return (
     <div className={leftNav['left-nav']}>
       <header className={leftNav['left-nav-header']}>
         <img src={logo} alt='logo' />
       </header>
       <Menu
-        defaultSelectedKeys={['1']}
-        defaultOpenKeys={['sub1']}
+        selectedKeys={[pathname]}
+        defaultOpenKeys={[subKey]}
         mode="inline"
         theme='light'
       // inlineCollapsed={this.state.collapsed}
       >
-        <Menu.Item key="1" icon={<PieChartOutlined />}>
+        <Menu.Item key='/home' icon={<PieChartOutlined />}>
           <Link to='/home'>
             首页
           </Link>
         </Menu.Item>
         <SubMenu key="sub1" icon={<MailOutlined />} title="商品">
-          <Menu.Item key="2">
+          <Menu.Item key='/category'>
             <Link to='/category'>
               品类管理
             </Link>
           </Menu.Item>
-          <Menu.Item key="3">
+          <Menu.Item key='/product'>
             <Link to='/product'>
               商品管理
             </Link>
           </Menu.Item>
         </SubMenu>
-        <Menu.Item key="4" icon={<DesktopOutlined />}>
+        <Menu.Item key='/user' icon={<DesktopOutlined />}>
           <Link to='/user'>
             用户管理
           </Link>
         </Menu.Item>
-        <Menu.Item key="5" icon={<ContainerOutlined />}>
+        <Menu.Item key='/role' icon={<ContainerOutlined />}>
           <Link to='/role'>
             角色管理
           </Link>
         </Menu.Item>
         <SubMenu key="sub2" icon={<AppstoreOutlined />} title="图形图表">
-          <Menu.Item key="6">
+          <Menu.Item key='/chart/bar'>
             <Link to='/chart/bar'>
               柱状图
             </Link>
           </Menu.Item>
-          <Menu.Item key="7">
+          <Menu.Item key='/chart/line'>
             <Link to='/chart/line'>
               折线图
             </Link>
           </Menu.Item>
-          <Menu.Item key="8">
+          <Menu.Item key='/chart/pie'>
             <Link to='/chart/pie'>
               饼图
             </Link>
           </Menu.Item>
         </SubMenu>
-        <Menu.Item key="9">
+        <Menu.Item key='/order'>
           <Link to='/order'>
             订单管理
           </Link>
---
+++ src/config/menuConfig.ts
@@ -0,0 +1,7 @@
+const menu = [{
+  title: '首页',
+  key: '/home',
+  icon: ''
+},];
+
+export default menu;
