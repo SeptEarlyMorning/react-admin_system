c26df00596419f3a5b29def8fd39ddc61c17129f
SeptEarlyMorning <1790732099@qq.com>
2020-06-06 23:54:03 +0800

    获取天气数据（高德地图）

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,8 +3,9 @@
 每个函数的返回值都是 promise
 */
 import ajax from './ajax';
+import jsonp from 'jsonp';
 import { AxiosResponse } from 'axios';
-
+import { message } from 'antd';
 // interface IReq {
 //   ([a: string]: any): Promise<AxiosResponse>
 // }
@@ -15,3 +16,31 @@ export const reqLogin = (username: string, password: string): Promise<AxiosRespo
 
 // 添加用户
 export const reqAddUser = (user: {}) => ajax('/manage/user/add', user, 'POST');
+
+// 查询位置信息
+export const reqLocation = () => {
+  return new Promise((resolve, reject) => {
+    jsonp('https://restapi.amap.com/v3/ip?output=json&key=1eddb5b19aecdcc6bf048018f6bf00a3', (error, data) => {
+      if (!error && data.status === '1') {
+        const { adcode } = data;
+        resolve({ adcode });
+      } else {
+        message.error('获取位置信息失败');
+      }
+    });
+  });
+};
+
+// 查询当前天气信息
+export const reqWeather = (adcode: string) => {
+  return new Promise((resolve, reject) => {
+    jsonp(`https://restapi.amap.com/v3/weather/weatherInfo?city=${adcode}&key=1eddb5b19aecdcc6bf048018f6bf00a3`, (error, data) => {
+      if (!error && data.status === '1') {
+        const { weather, temperature } = data.lives[0];
+        resolve({ weather, temperature });
+      } else {
+        message.error('获取天气信息失败');
+      }
+    });
+  });
+};
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
+import { reqLocation, reqWeather } from '../../api';
 
 const header = require('./header.module.less')
 
@@ -7,14 +8,27 @@ interface IProps {
 };
 
 function Header(props: IProps) {
+  const [weatherInfo, setWeatherInfo] = useState({ weather: '晴', temperature: 24 });
+
+  const getWeather = async () => {
+    const { adcode }: any = await reqLocation();
+    const { weather, temperature }: any = await reqWeather(adcode);
+    setWeatherInfo({ weather, temperature });
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
 
   return (
     <div className={header.header}>
       <div className={header['header-top']}>
-        <a href="javascript:;">退出</a>
         <span>欢迎，{'admin'}</span>
+        <a href="javascript(0):;">退出</a>
       </div>
       <div className={header['header-bottom']}>
+        {weatherInfo.temperature}
+        {weatherInfo.weather}
       </div>
     </div>
   );
